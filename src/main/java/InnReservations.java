a4051a2f24c53f18f3fe50833559ddb393a89f8a
Syrsha Harvey <syharvey@calpoly.edu>
2020-06-12 13:13:58 -0700

    lets turn this shit in

--- src/main/java/InnReservations.java
+++ src/main/java/InnReservations.java
@@ -1,3 +1,5 @@
+// CSC 365 Lab 7
+// Syrsha Harvey and Kattia Chang-Kam
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Connection;
@@ -38,7 +40,6 @@ public class InnReservations {
 	    InnReservations ir = new InnReservations();
             ir.initDb();
 	    ir.promptUser();
-	    //ir.demo3();
 	} catch (SQLException e) {
 	    System.err.println("SQLException: " + e.getMessage());
 	} catch (InputMismatchException ime) {
@@ -450,112 +451,6 @@ private int getWeekEndDays(LocalDate in, LocalDate out){
 	    }
 	}
     }
-
-    private void demo1() throws SQLException {
-
-	// Step 0: Load JDBC Driver
-	// No longer required as of JDBC 2.0  / Java 6
-	try{
-	    Class.forName("org.h2.Driver");
-	    System.out.println("H2 JDBC Driver loaded");
-	} catch (ClassNotFoundException ex) {
-	    System.err.println("Unable to load JDBC Driver");
-	    System.exit(-1);
-	}
-
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    // Step 2: Construct SQL statement
-	    String sql = "ALTER TABLE hp_goods ADD COLUMN AvailUntil DATE";
-
-	    // Step 3: (omitted in this example) Start transaction
-
-	    try (Statement stmt = conn.createStatement()) {
-
-		// Step 4: Send SQL statement to DBMS
-		boolean exRes = stmt.execute(sql);
-		
-		// Step 5: Handle results
-		System.out.format("Result from ALTER: %b %n", exRes);
-	    }
-
-	    // Step 6: (omitted in this example) Commit or rollback transaction
-	}
-	// Step 7: Close connection (handled by try-with-resources syntax)
-    }
-    
-
-    // Demo2 - Establish JDBC connection, execute SELECT query, read & print result
-    private void demo2() throws SQLException {
-
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    // Step 2: Construct SQL statement
-	    String sql = "SELECT * FROM hp_goods";
-
-	    // Step 3: (omitted in this example) Start transaction
-
-	    // Step 4: Send SQL statement to DBMS
-	    try (Statement stmt = conn.createStatement();
-		 ResultSet rs = stmt.executeQuery(sql)) {
-
-		// Step 5: Receive results
-		while (rs.next()) {
-		    String flavor = rs.getString("Flavor");
-		    String food = rs.getString("Food");
-		    float price = rs.getFloat("Price");
-		    System.out.format("%s %s ($%.2f) %n", flavor, food, price);
-		}
-	    }
-
-	    // Step 6: (omitted in this example) Commit or rollback transaction
-	}
-	// Step 7: Close connection (handled by try-with-resources syntax)
-    }
-
-    // Demo4 - Establish JDBC connection, execute DML query (UPDATE) using PreparedStatement / transaction    
-    private void demo4() throws SQLException {
-
-	// Step 1: Establish connection to RDBMS
-	try (Connection conn = DriverManager.getConnection(JDBC_URL,
-							   JDBC_USER,
-							   JDBC_PASSWORD)) {
-	    // Step 2: Construct SQL statement
-	    Scanner scanner = new Scanner(System.in);
-	    System.out.print("Enter a flavor: ");
-	    String flavor = scanner.nextLine();
-	    System.out.format("Until what date will %s be available (YYYY-MM-DD)? ", flavor);
-	    LocalDate availDt = LocalDate.parse(scanner.nextLine());
-	    
-	    String updateSql = "UPDATE hp_goods SET AvailUntil = ? WHERE Flavor = ?";
-
-	    // Step 3: Start transaction
-	    conn.setAutoCommit(false);
-	    
-	    try (PreparedStatement pstmt = conn.prepareStatement(updateSql)) {
-		
-		// Step 4: Send SQL statement to DBMS
-		pstmt.setDate(1, java.sql.Date.valueOf(availDt));
-		pstmt.setString(2, flavor);
-		int rowCount = pstmt.executeUpdate();
-		
-		// Step 5: Handle results
-		System.out.format("Updated %d records for %s pastries%n", rowCount, flavor);
-
-		// Step 6: Commit or rollback transaction
-		conn.commit();
-	    } catch (SQLException e) {
-		conn.rollback();
-	    }
-
-	}
-	// Step 7: Close connection (handled implcitly by try-with-resources syntax)
-    }
-
 
     private void initDb() throws SQLException {
 	try (Connection conn = DriverManager.getConnection(JDBC_URL,
